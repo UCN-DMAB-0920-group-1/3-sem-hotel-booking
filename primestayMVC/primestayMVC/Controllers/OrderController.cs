f1e75a43593381566f88bfc7ed2654383d7b8c07
nicolasmeller <56871907+nicolasmeller@users.noreply.github.com>
2021-10-06 12:18:22 +0200

    added views

---
+++ primestayMVC/primestayMVC/Controllers/OrderController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace primestayMVC.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly ILogger<OrderController> _logger;
+        public OrderController(ILogger<OrderController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
