7c15b2d48501ded8b7a09e4093564c509365df9b
MasterMidi <michael-graversen@hotmail.com>
2021-10-06 13:29:13 +0200

    Enviroment variables

---
+++ PrimeStayApi/PrimeStayApi.Enviroment/ENV_EXAMPLE.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrimeStayApi.Enviroment
+{
+
+    public class ENV_EXAMPLE
+    {
+        public const string connectionString = "Your-connection-string";
+        public const string connectionStringTest = "Your-connection-string";
+    }
+
+}
