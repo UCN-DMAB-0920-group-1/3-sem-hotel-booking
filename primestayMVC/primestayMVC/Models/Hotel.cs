03a6e61c6241d9c7cd83ac91de3ae2e73657dbc2
nicolasmeller <56871907+nicolasmeller@users.noreply.github.com>
2021-10-11 08:27:00 +0200

    some ocde

---
+++ primestayMVC/primestayMVC/Models/Hotel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace primestayMVC.Models
+{
+    public class Hotel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Staffed_hours { get; set; }
+        public int Stars { get; set; }
+    }
+}
