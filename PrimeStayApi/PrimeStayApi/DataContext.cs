9897b6c47b74060ebca6d029697252104a3ea212
Michael Andreas Graversen <michael-graversen@hotmail.com>
2021-10-16 21:09:55 +0200

    Added comments

--- PrimeStayApi/PrimeStayApi/DataContext.cs
+++ PrimeStayApi/PrimeStayApi/DataContext.cs
@@ -11,9 +11,12 @@ namespace PrimeStayApi
 {
     public class DataContext : IDataContext
     {
-
+        
         public IDbConnection OpenConnection()
         {
+            // ADO.NET creates a pool for the specific connectionstring supplied ot the contructor og the sqlconnection object.
+            // There is no need to worry about it opening lots of connection, everytime the method is called.
+            // Remember to call connection.Close() og connection.Dispose() after use, so that the connection will be returned to the connection pool.
             IDbConnection conn = new SqlConnection(ENV.ConnectionString);
             conn.Open();
             return conn;
