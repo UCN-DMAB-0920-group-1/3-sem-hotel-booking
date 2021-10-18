eeb44a7fd5931304932c53a317ace0bfbea6c97c
Magnus Baggesen <42898150+magnusBag@users.noreply.github.com>
2021-10-18 11:09:41 +0200

    API modelLayer

--- PrimeStayApi/PrimeStayApi.model/DTO/BaseModelDTO.cs
+++ PrimeStayApi/PrimeStayApi.model/DTO/BaseModelDTO.cs
@@ -3,9 +3,6 @@
     public class BaseModelDTO
     {
         public string Href {  get; set; }
-
-        public BaseModelDTO(int id) => this.Href = $"api/Hotel?Id={id}";
-
         public BaseModelDTO()
         {
         }
---
+++ PrimeStayApi/PrimeStayApi.model/DTO/LocationDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeStayApi.Model.DTO
+{
+    public class LocationDTO : BaseModelDTO
+    {
+        public string Street_Address { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Zip_code { get; set; }
+
+        public LocationDTO() : base()
+        {
+
+        }
+    }
+}
