67c93bdeb0562bab288b4c6e748d911683a3d179
nicolasmeller <56871907+nicolasmeller@users.noreply.github.com>
2021-10-18 11:12:07 +0200

    location mvc

---
+++ primestayMVC/primestayMVC/Controllers/LocationController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using primestayMVC.Model;
+
+namespace primestayMVC.Controllers
+{
+    public class LocationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        private Location GetLocation(string href)
+        {
+            RestClient client = new("https://localhost:44312/");
+            RestRequest request = new(href, Method.GET, DataFormat.Json);
+            return client.Execute<Location>(request).Data;
+
+        }
+    }
+}
