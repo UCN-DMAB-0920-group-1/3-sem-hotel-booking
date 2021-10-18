83dc503bde43d6ca90867969d89ad9d19bdc14b7
MasterMidi <michael-graversen@hotmail.com>
2021-10-18 11:09:11 +0200

    Update Hotel.cs

--- primestayMVC/primestayMVC/Model/Hotel.cs
+++ primestayMVC/primestayMVC/Model/Hotel.cs
@@ -3,11 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace primestayMVC.Models
+namespace primestayMVC.Model
 {
-    public class Hotel
+    public class Hotel : BaseModel
     {
-        public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string Staffed_hours { get; set; }
