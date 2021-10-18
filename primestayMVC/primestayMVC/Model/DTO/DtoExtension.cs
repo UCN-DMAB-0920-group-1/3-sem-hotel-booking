1b7b740cc50bedf5c10d18ae13c2456d7cb19e62
MasterMidi <michael-graversen@hotmail.com>
2021-10-18 12:26:34 +0200

    MVC dto mapping

---
+++ primestayMVC/primestayMVC/Model/DTO/DtoExtension.cs
@@ -0,0 +1,17 @@
+﻿namespace primestayMVC.Model.DTO
+{
+    public static class DtoExtension
+    {
+        public static int? ExtractId(this BaseDto dto)
+        {
+            return GetIdFromHref(dto.Href);
+        }
+
+        public static int? GetIdFromHref(string href)
+        {
+            if (string.IsNullOrEmpty(href)) return null;
+
+            return int.Parse(href[(href.LastIndexOf("/") + 1)..]);
+        }
+    }
+}
