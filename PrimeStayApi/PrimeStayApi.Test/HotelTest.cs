5573530f8bc09a1a5d3d4deb7341100d3bff6f54
Magnus Baggesen <42898150+magnusBag@users.noreply.github.com>
2021-10-19 12:16:26 +0200

    Test on HotelController begun

--- PrimeStayApi/PrimeStayApi.Test/HotelTest.cs
+++ PrimeStayApi/PrimeStayApi.Test/HotelTest.cs
@@ -4,42 +4,97 @@ using PrimeStayApi.DataAccessLayer;
 using PrimeStayApi.Database;
 using PrimeStayApi.Enviroment;
 using PrimeStayApi.Model;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace PrimeStayApi.Test
 {
     [TestClass]
     public class HotelTest
     {
-        private HotelController _controller;
+        private HotelController _controllerWithDB;
         private IDao<Hotel> _dao;
         private DataContext _dataContext;
+        private HotelController _controllerNoDB;
 
 
         [TestInitialize]
-        private void SetUp()
+        public void SetUp()
         {
-            _dataContext = new DataContext(ENV.ConnectionStringTest);
-            _dao = DaoFactory.Create<Hotel>(_dataContext);
-            _controller = new HotelController(_dao);
             Version.Upgrade(ENV.ConnectionStringTest);
+            _dataContext = new DataContext(ENV.ConnectionStringTest);
+
         }
         [TestCleanup]
-        private void CleanUp()
+        public void CleanUp()
         {
             Version.Drop(ENV.ConnectionStringTest);
         }
 
 
         [TestMethod]
-        public void GetHotelWithId1()
+        public void GetHotelFromTestDBWithId()
         {
             //arrange
+            _dao = DaoFactory.Create<Hotel>(_dataContext);
+            _controllerWithDB = new HotelController(_dao);
             int hotelId = 1;
             //act 
-            var hotel = _controller.Details(hotelId);
+            var hotel = _controllerWithDB.Details(hotelId);
             //assert 
             Assert.IsTrue(hotel.Name.Equals("Hotel Petrús"));
+        }
 
+        [TestMethod]
+        public void GetHotelFromDBWithHotel()
+        {
+            //arrange
+            _dao = DaoFactory.Create<Hotel>(_dataContext);
+            _controllerWithDB = new HotelController(_dao);
+            var hotel = new Hotel()
+            {
+                Name = "Hotel Petrús",
+                Description = "Classic old fashioned hotel with a river of red wine.",
+                Stars = 3,
+                Staffed_hours = "24/7",
+            };
+            //act 
+            var hotels = _controllerWithDB.Index(hotel.Name, hotel.Description, hotel.Staffed_hours, hotel.Stars);
+            //assert 
+            Assert.IsTrue(hotels.Count() == 1);
+            Assert.IsTrue(hotels.First().Name == hotel.Name);
+            Assert.IsTrue(hotels.First().Description == hotel.Description);
+            Assert.IsTrue(hotels.First().Staffed_hours == hotel.Staffed_hours);
+            Assert.IsTrue(hotels.First().Stars == hotel.Stars);
+        }
+
+    }
+
+    internal class FakeHotelDao : IDao<Hotel>
+    {
+        public int Create(Hotel model)
+        {
+            return -1;
+        }
+
+        public int Delete(Hotel model)
+        {
+            return -1;
+        }
+
+        public IEnumerable<Hotel> ReadAll(Hotel model)
+        {
+            return new List<Hotel>();
+        }
+
+        public Hotel ReadById(int id)
+        {
+            return new Hotel();
+        }
+
+        public int Update(Hotel model)
+        {
+            return -1;
         }
     }
 }
