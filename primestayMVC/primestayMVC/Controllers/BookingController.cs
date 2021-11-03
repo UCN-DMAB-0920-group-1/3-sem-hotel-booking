d34865454f072e563f769f576f25024100625482
Magnus Baggesen <42898150+magnusBag@users.noreply.github.com>
2021-11-03 15:18:25 +0100

    bug fix

--- primestayMVC/primestayMVC/Controllers/BookingController.cs
+++ primestayMVC/primestayMVC/Controllers/BookingController.cs
@@ -1,15 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using primestay.MVC.Model;
 using PrimeStay.MVC.DataAccessLayer;
 using PrimeStay.MVC.DataAccessLayer.DTO;
 using PrimeStay.MVC.Model;
 using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace PrimeStay.MVC.Controllers
 {
@@ -42,7 +36,7 @@ namespace PrimeStay.MVC.Controllers
             {
                 Start_date = DateTime.Parse(HttpContext.Session.GetString("checkIn") + "Z"),
                 End_date = DateTime.Parse(HttpContext.Session.GetString("checkOut") + "Z"),
-                Num_of_guests = HttpContext.Session.GetInt32("guests"),
+                Num_of_guests = int.Parse(HttpContext.Session.GetString("guests")),
                 Room_href = HttpContext.Session.GetString("selectedRoom"),
                 Customer_href = "api/cutomer/1", //TODO find actual customer ;-) 
 
