40d219e526a7a6a58bd4c60faaca3e0d4d494710
nicolasmeller <56871907+nicolasmeller@users.noreply.github.com>
2021-11-04 10:28:48 +0100

    test on pictures

---
+++ PrimeStayApi/PrimeStayApi.Test/PictureTest.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PrimeStayApi.Controllers;
+using PrimeStayApi.DataAccessLayer;
+using PrimeStayApi.DataAccessLayer.DAO;
+using PrimeStayApi.Database;
+using PrimeStayApi.Enviroment;
+using PrimeStayApi.Model;
+using PrimeStayApi.Model.DTO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PrimeStayApi.Test
+{
+
+    [TestClass]
+    public class PictureTest
+    {
+        private PictureController _controllerWithDB;
+        private IDao<PictureEntity> _dao;
+        private DataContext _dataContext;
+        private PictureController _controllerNoDB;
+       
+
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            Version.Upgrade(ENV.ConnectionStringTest);
+            _dataContext = new DataContext(ENV.ConnectionStringTest);
+
+        }
+        [TestCleanup]
+        public void CleanUp()
+        {
+            Version.Drop(ENV.ConnectionStringTest);
+        }
+
+
+        [TestMethod]
+        public void GetRoomPictureTest()
+        {
+            //arrange
+            _dao = DaoFactory.Create<PictureEntity>(_dataContext);
+            _controllerWithDB = new PictureController(_dao);
+            PictureDto[] pictureDtos = { };
+            string type = "room";
+            int id = 1;
+
+
+            //act 
+            pictureDtos = _controllerWithDB.getPictureByType(type, id).ToArray();
+
+            //assert 
+            Assert.IsNotNull(pictureDtos);
+            Assert.IsTrue(pictureDtos.Length == 3);
+            Assert.IsTrue(pictureDtos[0].Path == "https://juto.dk/semester/room/1.png");
+            Assert.IsTrue(pictureDtos[1].Path == "https://juto.dk/semester/room/2.png");
+            Assert.IsTrue(pictureDtos[2].Path == "https://juto.dk/semester/room/3.png");
+        }
+        [TestMethod]
+        public void GetHotelPictureTest()
+        {
+            //arrange
+            _dao = DaoFactory.Create<PictureEntity>(_dataContext);
+            _controllerWithDB = new PictureController(_dao);
+            PictureDto[] pictureDtos = { };
+            string type = "hotel";
+            int id = 1;
+
+
+            //act 
+            pictureDtos = _controllerWithDB.getPictureByType(type, id).ToArray();
+
+            //assert 
+            Assert.IsNotNull(pictureDtos);
+            Assert.IsTrue(pictureDtos.Length == 3);
+            Assert.IsTrue(pictureDtos[0].Path == "https://juto.dk/semester/hotel/1.png");
+            Assert.IsTrue(pictureDtos[1].Path == "https://juto.dk/semester/hotel/2.png");
+            Assert.IsTrue(pictureDtos[2].Path == "https://juto.dk/semester/hotel/3.png");
+        }
+    }
+}
