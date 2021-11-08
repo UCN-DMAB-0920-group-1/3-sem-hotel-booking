65c887e20b4c58154b5510b0f2892cf3f8440de0
MasterMidi <michael-graversen@hotmail.com>
2021-11-08 12:38:35 +0100

    Fixed tests

--- PrimeStayApi/PrimeStayApi.model/DTO/PictureDto.cs
+++ PrimeStayApi/PrimeStayApi.model/DTO/PictureDto.cs
@@ -6,10 +6,10 @@ using System.Threading.Tasks;
 
 namespace PrimeStayApi.Model.DTO
 {
-    public class PictureDto:BaseModelDto
+    public class PictureDto : BaseModelDto
     {
-        public string? RoomHref { get; set; }
-        public string? HotelHref { get; set; }
+        public string RoomHref { get; set; }
+        public string HotelHref { get; set; }
         public string Path { get; set; }
         public string Description { get; set; }
         public string Title { get; set; }
