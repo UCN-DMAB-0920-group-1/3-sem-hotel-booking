8f2e1066716a9bd9330a5d3960de76ac7515f7d0
Magnus Baggesen <42898150+magnusBag@users.noreply.github.com>
2021-11-09 13:00:59 +0100

    Spelling changes

--- primestayMVC/primestayMVC/Controllers/BookingController.cs
+++ primestayMVC/primestayMVC/Controllers/BookingController.cs
@@ -36,8 +36,8 @@ namespace PrimeStay.MVC.Controllers
             {
                 Start_date = DateTime.Parse(HttpContext.Session.GetString("startDate") + "Z"),
                 End_date = DateTime.Parse(HttpContext.Session.GetString("endDate") + "Z"),
-                Num_of_guests = int.Parse(HttpContext.Session.GetString("guests")),
-                Room_Type_href = HttpContext.Session.GetString("selectedRoom"),
+                Guests = int.Parse(HttpContext.Session.GetString("guests")),
+                Room_type_href = HttpContext.Session.GetString("selectedRoom"),
                 Customer_href = "api/cutomer/1", //TODO find actual customer ;-) 
 
 
--- primestayMVC/primestayMVC/Model/ModelMapper.cs
+++ primestayMVC/primestayMVC/Model/ModelMapper.cs
@@ -98,8 +98,8 @@ namespace PrimeStay.MVC.Model
                 EndDate = booking.End_date,
                 StartDate = booking.Start_date,
                 CustomerHref = booking.Customer_href,
-                Guests = booking.Num_of_guests,
-                RoomTypeHref = booking.Room_Type_href,
+                Guests = booking.Guests,
+                RoomTypeHref = booking.Room_type_href,
             };
         }
 
@@ -113,8 +113,8 @@ namespace PrimeStay.MVC.Model
                 Customer_href = booking.CustomerHref,
                 End_date = booking.EndDate,
                 Start_date = booking.StartDate,
-                Num_of_guests = booking.Guests,
-                Room_Type_href = booking.RoomTypeHref,
+                Guests = booking.Guests,
+                Room_type_href = booking.RoomTypeHref,
 
             };
         }
