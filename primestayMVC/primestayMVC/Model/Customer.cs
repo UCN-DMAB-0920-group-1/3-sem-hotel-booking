df477a8be183ea609dfa2c2cc84a79121768f379
magnusBag <42898150+magnusBag@users.noreply.github.com>
2021-11-11 19:05:47 +0100

    Update Customer.cs

--- primestayMVC/primestayMVC/Model/Customer.cs
+++ primestayMVC/primestayMVC/Model/Customer.cs
@@ -5,7 +5,7 @@
         public string Name { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
-        public int? Birthday { get; set; }
+        public DateTime Birthday { get; set; }
 
     }
 }
