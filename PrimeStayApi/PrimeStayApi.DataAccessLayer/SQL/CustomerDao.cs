bbaee3e59d83fab026f138538258c94e045ed2a7
Magnus Baggesen <42898150+magnusBag@users.noreply.github.com>
2021-11-22 10:42:17 +0100

    Customer Dto

--- PrimeStayApi/PrimeStayApi.DataAccessLayer/SQL/CustomerDao.cs
+++ PrimeStayApi/PrimeStayApi.DataAccessLayer/SQL/CustomerDao.cs
@@ -11,9 +11,9 @@ namespace PrimeStayApi.DataAccessLayer.SQL
     internal class CustomerDao : BaseDao<IDataContext<IDbConnection>>, IDao<CustomerEntity>
     {
         #region SQL-Queries
-        private static readonly string SELECTBOOKINGBYID = @"SELECT * FROM Booking WHERE id = @id";
+        private static readonly string SELECTCUSTOMERBYID = @"SELECT * FROM Customer WHERE id = @id";
 
-        private static readonly string SELECTALLBOOKINGS = @"SELECT * FROM Booking WHERE " +
+        private static readonly string SELECTALLCUSTOMER = @"SELECT * FROM Booking WHERE " +
                                                             "id = ISNULL(@id,id)" +
                                                             "AND start_date >= ISNULL(@Start_date, Start_date)" +
                                                             "AND end_date <= ISNULL(@End_date, End_date)" +
@@ -21,26 +21,11 @@ namespace PrimeStayApi.DataAccessLayer.SQL
                                                             "AND room_id = ISNULL(@Room_id, Room_id)" +
                                                             "AND customer_id = ISNULL(@Customer_id, Customer_id)";
 
-        private static readonly string INSERTBOOKINGRETURNID = @"INSERT INTO Booking (Start_date, End_date, Guests,Room_id,Customer_id) " +
+        private static readonly string INSERTCUSTOMERRETURNID = @"INSERT INTO Booking (Start_date, End_date, Guests,Room_id,Customer_id) " +
                                                                 @"OUTPUT INSERTED.id " +
                                                                 @"VALUES (@Start_date, @End_date, @Guests,@Room_id,@Customer_id)";
 
-        private static readonly string GETAVAILABLEROOMRANDOM = "SELECT TOP 1 Room.id FROM Room " +
-                                                                "WHERE room.room_type_id = @Room_type_id AND Room.id NOT IN " +
-                                                                "( " +
-                                                                    "SELECT R.id " +
-                                                                    "FROM  Booking B " +
-                                                                        "JOIN Room R " +
-                                                                        "ON B.room_id = R.id " +
-                                                                        "WHERE " +
-                                                                        "( " +
-                                                                            "(B.start_date <= @start_date AND B.end_date >= @start_date) " +
-                                                                            "OR (B.start_date < @end_date AND B.end_date >= @end_date) " +
-                                                                            "OR (@start_date <= B.start_date AND @end_date >= B.start_date)" +
-                                                                        ") " +
-                                                                        "AND Room.room_type_id = @room_type_id " +
-                                                                ") " +
-                                                                "ORDER BY NEWID()";
+
         #endregion
         public CustomerDao(IDataContext<IDbConnection> dataContext) : base(dataContext)
         {
@@ -61,7 +46,7 @@ namespace PrimeStayApi.DataAccessLayer.SQL
         {
             using (IDbConnection connection = DataContext.Open())
             {
-                return connection.Query<CustomerEntity>(SELECTALLBOOKINGS, model);
+                return connection.Query<CustomerEntity>(SELECTALLCUSTOMER, model);
 
             };
         }
@@ -70,7 +55,7 @@ namespace PrimeStayApi.DataAccessLayer.SQL
         {
             using (IDbConnection connection = DataContext.Open())
             {
-                return connection.QueryFirst<CustomerEntity>(SELECTBOOKINGBYID, new { id });
+                return connection.QueryFirst<CustomerEntity>(SELECTCUSTOMERBYID, new { id });
             }
         }
 
--- PrimeStayApi/PrimeStayApi.model/DTO/CustomerDto.cs
+++ PrimeStayApi/PrimeStayApi.model/DTO/CustomerDto.cs
@@ -1,9 +1,12 @@
-﻿namespace PrimeStayApi.Model.DTO
+﻿using System;
+
+namespace PrimeStayApi.Model.DTO
 {
     public class CustomerDto : BaseModelDto
     {
         public string Name { get; set; }
         public string Phone { get; set; }
         public string Email { get; set; }
+        public DateTime Birthday { get; set; }
     }
 }
