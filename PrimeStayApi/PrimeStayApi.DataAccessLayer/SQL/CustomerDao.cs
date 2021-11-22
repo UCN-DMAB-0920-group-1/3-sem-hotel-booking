c246e381e47d949126ca86a73a8f82afea4d0e7e
Magnus Baggesen <42898150+magnusBag@users.noreply.github.com>
2021-11-22 13:22:32 +0100

    Cusomer controller done

--- PrimeStayApi/PrimeStayApi.DataAccessLayer/SQL/CustomerDao.cs
+++ PrimeStayApi/PrimeStayApi.DataAccessLayer/SQL/CustomerDao.cs
@@ -4,6 +4,7 @@ using PrimeStayApi.DataAccessLayer.DAO;
 using PrimeStayApi.Model;
 using System.Collections.Generic;
 using System.Data;
+using System.Diagnostics;
 
 namespace PrimeStayApi.DataAccessLayer.SQL
 {
@@ -22,6 +23,9 @@ namespace PrimeStayApi.DataAccessLayer.SQL
         private static readonly string INSERTCUSTOMERRETURNID = @"INSERT INTO Customer (Name, Email, Phone,Birthday) " +
                                                                 @"OUTPUT INSERTED.id " +
                                                                 @"VALUES (@Name, @Email, @Phone,@Birthday)";
+        private static readonly string DELETECUSTOMER = "DELETE FROM Customer WHERE id=@id AND Name=@Name AND Phone=@phone AND Email=@Email";
+        private readonly static string UPDATECUSTOMER = "UPDATE Customer SET Name=@name ,Phone=@Phone, Email=@Email WHERE id=@id";
+
 
 
         #endregion
@@ -48,7 +52,20 @@ namespace PrimeStayApi.DataAccessLayer.SQL
 
         public int Delete(CustomerEntity model)
         {
-            throw new System.NotImplementedException();
+            int res = -1;
+            using (IDbConnection connection = DataContext.Open())
+            {
+                try
+                {
+                    res = connection.Execute(DELETECUSTOMER, model);
+                }
+                catch (System.Exception e)
+                {
+                    Debug.WriteLine(e);
+                    return res;
+                }
+            }
+            return res;
         }
 
         public IEnumerable<CustomerEntity> ReadAll(CustomerEntity model)
@@ -70,7 +87,20 @@ namespace PrimeStayApi.DataAccessLayer.SQL
 
         public int Update(CustomerEntity model)
         {
-            throw new System.NotImplementedException();
+            int res = -1;
+            using (IDbConnection connection = DataContext.Open())
+            {
+                try
+                {
+                    res = connection.Execute(UPDATECUSTOMER, model);
+                }
+                catch (System.Exception e)
+                {
+                    Debug.WriteLine(e);
+                    return res;
+                }
+                return res;
+            }
         }
     }
 }
