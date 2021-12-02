25bd3ec9db1995858cf972addd694088f9bff356
Michael Andreas Graversen <michael-graversen@hotmail.com>
2021-12-02 09:50:36 +0100

    Preliminary separation and optimization

---
+++ PrimeStayApi/Tests/Integration/BaseDbSetup.cs
@@ -0,0 +1,38 @@
+﻿using API;
+using Enviroment;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Version = Database.Version;
+
+namespace Tests.Integration
+{
+    public class BaseDbSetup
+    {
+        #region setup
+        internal string connectionString = new ENV().ConnectionStringTest;
+        internal static SqlDataContext _dataContext;
+        internal static List<Action> _dropDatabaseActions = new();
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _dataContext = new SqlDataContext(connectionString);
+            Version.Upgrade(connectionString);
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            Parallel.Invoke(_dropDatabaseActions.ToArray());
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            _dropDatabaseActions.Add(() => Version.Drop(connectionString));
+        }
+        #endregion
+    }
+}
