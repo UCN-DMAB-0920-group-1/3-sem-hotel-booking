2036bb243ec08e9030793c3fc3010f3f74f73478
Julius Bendt <julius@juto.dk>
2021-12-06 09:50:03 +0100

    Fixed tests (somewhat, at least) user_id on customer is now an int

--- PrimeStayApi/Database/Scripts/001_createtables.sql
+++ PrimeStayApi/Database/Scripts/001_createtables.sql
@@ -80,7 +80,7 @@ CREATE TABLE [Customer] (
   [email] VARCHAR(250) NOT NULL,
   [phone] VARCHAR(250) NOT NULL,
   [birthday] datetime NOT NULL,
-  [user_id] VARCHAR(250),
+  [user_id] int NOT NULL
   PRIMARY KEY ([id])
 )
 GO
